69bac4c8f5175dc0f8896336841bfe8348c8fc13
alex <voltov2017@yandex.ru>
2020-05-13 13:31:55 +0300

    add BeanConfig

--- src/main/java/com/sample/movie_catalog/controllers/MovieController.java
+++ src/main/java/com/sample/movie_catalog/controllers/MovieController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,10 +18,11 @@ import java.util.Objects;
 @RequestMapping("movies")
 public class MovieController {
     private final ApplicationProperties props;
+    private final RestTemplate restTemp;
 
     @GetMapping("")
     private String list(Model model) {
-        List<Movie> movies = Arrays.asList(Objects.requireNonNull(new RestTemplate().getForEntity(props.getList(), Movie[].class).getBody()));
+        List<Movie> movies = Arrays.asList(Objects.requireNonNull(restTemp.getForEntity(props.getList(), Movie[].class).getBody()));
         model.addAttribute("models", movies);
         return "index";
     }
