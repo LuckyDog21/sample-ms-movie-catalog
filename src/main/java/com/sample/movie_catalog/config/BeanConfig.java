3badc9fc3ba84b1a910ceceaa5e9382065ee3ab9
alex <voltov2017@yandex.ru>
2020-05-13 13:46:41 +0300

    upd and add webflux

--- src/main/java/com/sample/movie_catalog/config/BeanConfig.java
+++ src/main/java/com/sample/movie_catalog/config/BeanConfig.java
@@ -3,6 +3,7 @@ package com.sample.movie_catalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BeanConfig {
@@ -11,4 +12,10 @@ public class BeanConfig {
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    WebClient.Builder webClient(){
+        return WebClient.builder();
+    }
+
 }
--- src/main/java/com/sample/movie_catalog/controllers/MovieController.java
+++ src/main/java/com/sample/movie_catalog/controllers/MovieController.java
@@ -8,21 +8,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("movies")
 public class MovieController {
     private final ApplicationProperties props;
-    private final RestTemplate restTemp;
+    private final WebClient.Builder webClient;
 
     @GetMapping("")
     private String list(Model model) {
-        List<Movie> movies = Arrays.asList(Objects.requireNonNull(restTemp.getForEntity(props.getList(), Movie[].class).getBody()));
+        //one way with restTemplate
+        //List<Movie> movies = Arrays.asList(Objects.requireNonNull(restTemp.getForEntity(props.getList(), Movie[].class).getBody()));
+        List<Movie> movies = webClient.build().get()
+                .uri(props.getList())
+                .retrieve()
+                .bodyToFlux(Movie.class)
+                .buffer().blockFirst();
         model.addAttribute("models", movies);
         return "index";
     }
