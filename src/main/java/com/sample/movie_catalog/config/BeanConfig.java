ec65be72ff9e432d282bea96c1c0b4152baccffb
alex <voltov2017@yandex.ru>
2020-05-15 11:52:47 +0300

    add timeout for webclient and rest template

--- src/main/java/com/sample/movie_catalog/config/BeanConfig.java
+++ src/main/java/com/sample/movie_catalog/config/BeanConfig.java
@@ -1,23 +1,43 @@
 package com.sample.movie_catalog.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
     RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(300);
         return new RestTemplate();
     }
 
     @Bean
     @LoadBalanced //it's need for send some request to microservice by name (name from discovery server)
-    WebClient.Builder webClient(){
-        return WebClient.builder();
+    WebClient.Builder webClient() {
+        int connectTimeOut = 10_000;
+        long readTimeOut = 10_000L;
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(tcpClient -> {
+                    tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeOut);
+                    tcpClient = tcpClient.doOnConnected(conn -> conn
+                            .addHandlerLast(new ReadTimeoutHandler(readTimeOut, TimeUnit.MILLISECONDS)));
+                    return tcpClient;
+                });
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+        return WebClient.builder().clientConnector(connector);
     }
 
 }
